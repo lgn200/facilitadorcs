697a2ec208ee44a03b5f781d2d381c861cbd9fbf
bestsenai <bestsenai@1aa38525-cd51-86ae-f5ad-7c04be12d4f8>
2010-05-30 21:53:39 +0000

    Tela de Cadastro Clientes e tbm icones novos....

--- trunk/FacilitadorCS/src/View/TelaAberturaOS.java
+++ trunk/FacilitadorCS/src/View/TelaAberturaOS.java
@@ -71,37 +71,56 @@ public class TelaAberturaOS extends javax.swing.JFrame {
         setTitle("Abertura de Ordem de Serviço");
         setBounds(new java.awt.Rectangle(10, 10, 800, 600));
         setMinimumSize(new java.awt.Dimension(900, 700));
+        getContentPane().setLayout(null);
 
         cbAtendente.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbAtendente.setAutoscrolls(true);
+        getContentPane().add(cbAtendente);
+        cbAtendente.setBounds(90, 117, 226, 20);
 
-        lbAtendente.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbAtendente.setFont(new java.awt.Font("Arial", 0, 14));
         lbAtendente.setText("Atendente:");
         lbAtendente.setAutoscrolls(true);
+        getContentPane().add(lbAtendente);
+        lbAtendente.setBounds(90, 94, 69, 17);
 
-        lbTecnico.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbTecnico.setFont(new java.awt.Font("Arial", 0, 14));
         lbTecnico.setText("Técnico:");
         lbTecnico.setAutoscrolls(true);
+        getContentPane().add(lbTecnico);
+        lbTecnico.setBounds(380, 94, 54, 17);
 
         cbTecnico.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbTecnico.setAutoscrolls(true);
+        getContentPane().add(cbTecnico);
+        cbTecnico.setBounds(380, 117, 226, 20);
 
         lbTituloAbreOS.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         lbTituloAbreOS.setText("Controle de Ordem de Serviço");
         lbTituloAbreOS.setAutoscrolls(true);
+        getContentPane().add(lbTituloAbreOS);
+        lbTituloAbreOS.setBounds(560, 10, 215, 36);
 
-        lbCliente.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbCliente.setFont(new java.awt.Font("Arial", 0, 14));
         lbCliente.setText("Cliente:");
         lbCliente.setAutoscrolls(true);
+        getContentPane().add(lbCliente);
+        lbCliente.setBounds(90, 147, 48, 17);
 
         cbCliente.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbCliente.setAutoscrolls(true);
+        getContentPane().add(cbCliente);
+        cbCliente.setBounds(90, 180, 353, 22);
 
         btBuscaClienteOS.setText("Busca");
         btBuscaClienteOS.setAutoscrolls(true);
+        getContentPane().add(btBuscaClienteOS);
+        btBuscaClienteOS.setBounds(460, 180, 61, 23);
 
         btNovoClienteOS.setText("Novo");
         btNovoClienteOS.setAutoscrolls(true);
+        getContentPane().add(btNovoClienteOS);
+        btNovoClienteOS.setBounds(528, 180, 61, 23);
 
         spDescricaoOS.setAutoscrolls(true);
 
@@ -109,29 +128,54 @@ public class TelaAberturaOS extends javax.swing.JFrame {
         taDescricaoOS.setRows(5);
         spDescricaoOS.setViewportView(taDescricaoOS);
 
-        lbDataOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        getContentPane().add(spDescricaoOS);
+        spDescricaoOS.setBounds(90, 244, 504, 103);
+
+        lbDataOS.setFont(new java.awt.Font("Arial", 0, 14));
         lbDataOS.setText("Data Abertura:");
         lbDataOS.setAutoscrolls(true);
+        getContentPane().add(lbDataOS);
+        lbDataOS.setBounds(616, 244, 93, 17);
+        getContentPane().add(txDataOS);
+        txDataOS.setBounds(616, 267, 123, 20);
 
-        lbHoraOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbHoraOS.setFont(new java.awt.Font("Arial", 0, 14));
         lbHoraOS.setText("Hora de Abertura");
         lbHoraOS.setAutoscrolls(true);
+        getContentPane().add(lbHoraOS);
+        lbHoraOS.setBounds(616, 293, 109, 17);
+        getContentPane().add(txHoraOS);
+        txHoraOS.setBounds(620, 320, 123, 20);
 
-        lbDescricaoOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbDescricaoOS.setFont(new java.awt.Font("Arial", 0, 14));
         lbDescricaoOS.setText("Descrição:");
         lbDescricaoOS.setAutoscrolls(true);
+        getContentPane().add(lbDescricaoOS);
+        lbDescricaoOS.setBounds(90, 221, 67, 17);
 
         lbNumOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         lbNumOS.setText("Nº O.S.");
         lbNumOS.setAutoscrolls(true);
+        getContentPane().add(lbNumOS);
+        lbNumOS.setBounds(90, 51, 46, 17);
+        getContentPane().add(txNumOS);
+        txNumOS.setBounds(154, 48, 69, 20);
 
-        lbDescontoOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbDescontoOS.setFont(new java.awt.Font("Arial", 0, 14));
         lbDescontoOS.setText("Desconto:");
         lbDescontoOS.setAutoscrolls(true);
+        getContentPane().add(lbDescontoOS);
+        lbDescontoOS.setBounds(547, 477, 90, 17);
+        getContentPane().add(txDescontoOS);
+        txDescontoOS.setBounds(547, 510, 97, 20);
 
-        lbTotalOS.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lbTotalOS.setFont(new java.awt.Font("Arial", 0, 14));
         lbTotalOS.setText("Total a Pagar:");
         lbTotalOS.setAutoscrolls(true);
+        getContentPane().add(lbTotalOS);
+        lbTotalOS.setBounds(657, 477, 90, 17);
+        getContentPane().add(txTotalOS);
+        txTotalOS.setBounds(657, 510, 97, 20);
 
         spItemOS.setAutoscrolls(true);
 
@@ -163,6 +207,9 @@ public class TelaAberturaOS extends javax.swing.JFrame {
             }
         ));
         spItemOS.setViewportView(tbItemOS);
+
+        getContentPane().add(spItemOS);
+        spItemOS.setBounds(90, 365, 664, 94);
 
         tbAberturaOS.setFloatable(false);
         tbAberturaOS.setOrientation(1);
@@ -253,141 +300,16 @@ public class TelaAberturaOS extends javax.swing.JFrame {
         btFecharAberOS.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         tbAberturaOS.add(btFecharAberOS);
 
+        getContentPane().add(tbAberturaOS);
+        tbAberturaOS.setBounds(0, 0, 72, 572);
+
         pbStatusAbertOS.setAutoscrolls(true);
+        getContentPane().add(pbStatusAbertOS);
+        pbStatusAbertOS.setBounds(60, 580, 700, 20);
 
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(tbAberturaOS, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbCliente)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(cbCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(17, 17, 17)
-                        .addComponent(btBuscaClienteOS)
-                        .addGap(7, 7, 7)
-                        .addComponent(btNovoClienteOS))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(460, 460, 460)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txDescontoOS, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lbDescontoOS))
-                        .addGap(13, 13, 13)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbTotalOS)
-                            .addComponent(txTotalOS, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lbNumOS)
-                        .addGap(18, 18, 18)
-                        .addComponent(txNumOS, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(lbDescricaoOS)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addComponent(spDescricaoOS, javax.swing.GroupLayout.PREFERRED_SIZE, 504, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jSeparator1))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(cbAtendente, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(lbAtendente))
-                                .addGap(64, 64, 64)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(lbTecnico)
-                                    .addComponent(cbTecnico, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lbDataOS))
-                            .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lbHoraOS))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(txHoraOS, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(txDataOS, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(lbTituloAbreOS, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(spItemOS, javax.swing.GroupLayout.PREFERRED_SIZE, 664, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(50, 50, 50)
-                .addComponent(pbStatusAbertOS, javax.swing.GroupLayout.DEFAULT_SIZE, 735, Short.MAX_VALUE)
-                .addGap(24, 24, 24))
-        );
-
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btBuscaClienteOS, btNovoClienteOS});
-
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {lbDescontoOS, lbTotalOS});
-
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(49, 49, 49)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(txNumOS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(lbNumOS))
-                                .addGap(26, 26, 26)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(lbAtendente)
-                                    .addComponent(lbTecnico)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(lbTituloAbreOS, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(6, 6, 6)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(cbAtendente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(cbTecnico, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(10, 10, 10)
-                        .addComponent(lbCliente)
-                        .addGap(16, 16, 16)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(cbCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btBuscaClienteOS)
-                            .addComponent(btNovoClienteOS))
-                        .addGap(17, 17, 17)
-                        .addComponent(lbDescricaoOS)
-                        .addGap(6, 6, 6)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE)
-                            .addComponent(spDescricaoOS, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lbDataOS)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txDataOS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lbHoraOS)
-                                .addGap(11, 11, 11)
-                                .addComponent(txHoraOS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(spItemOS, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lbDescontoOS)
-                            .addComponent(lbTotalOS))
-                        .addGap(16, 16, 16)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txDescontoOS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txTotalOS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(53, 53, 53))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(tbAberturaOS, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
-                .addComponent(pbStatusAbertOS, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
+        getContentPane().add(jSeparator1);
+        jSeparator1.setBounds(597, 244, 1, 115);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
